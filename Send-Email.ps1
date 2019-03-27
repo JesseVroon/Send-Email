038e85afe6d0a883c6ef1a1a55df2133a527396d
Jesse Vroon <48052767+JesseVroon@users.noreply.github.com>
2019-03-27 11:44:30 +0100

    Update Send-Email.ps1

--- Send-Email.ps1
+++ Send-Email.ps1
@@ -1,16 +1,17 @@
-﻿#Name: Send-Email.ps1
+#Name: Send-Email.ps1
 #Script URL: https://github.com/JesseVroon/Send-Email/
 #Description: Sends Email
 #Version: 1.0.0
 #Author: Jesse Vroon
 #Author URL: https://github.com/JesseVroon/
 
-$From = "jesse.vroon@live.nl"
-$To = "jesse.vroon@live.nl"
-$Cc = "jesse.vroon@gmail.com"
+$From = "example@example.com"
+$To = "example@example.com"
+$Cc = "example@example.com"
 $Attachment = "C:\Users\jesse\OneDrive\Documents\PowerShell\PCinfo.ps1"
 $Subject = "Verzonden met Powershell"
-$Body = "test"
+$date = Get-Date
+$Body = ""Dit bericht is verzonden vanaf "+ $env:COMPUTERNAME +" op " + $date"
 $SMTPServer = "smtp.office365.com"
 $SMTPPort = "587"
 Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl -Credential (Get-Credential) -Attachments $Attachment –DeliveryNotificationOption OnSuccess
