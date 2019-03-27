9cfcbcf190a1ed92c487b0adafd4e16576609bed
Jesse Vroon <48052767+JesseVroon@users.noreply.github.com>
2019-03-27 11:17:11 +0100

    Add files via upload

---
+++ Send-Email.ps1
@@ -0,0 +1,16 @@
+﻿#Name: Send-Email.ps1
+#Script URL: https://github.com/JesseVroon/Send-Email/
+#Description: Sends Email
+#Version: 1.0.0
+#Author: Jesse Vroon
+#Author URL: https://github.com/JesseVroon/
+
+$From = "jesse.vroon@live.nl"
+$To = "jesse.vroon@live.nl"
+$Cc = "jesse.vroon@gmail.com"
+$Attachment = "C:\Users\jesse\OneDrive\Documents\PowerShell\PCinfo.ps1"
+$Subject = "Powershell"
+$Body = "test"
+$SMTPServer = "smtp.office365.com"
+$SMTPPort = "587"
+Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort -UseSsl -Credential (Get-Credential) -Attachments $Attachment –DeliveryNotificationOption OnSuccess
